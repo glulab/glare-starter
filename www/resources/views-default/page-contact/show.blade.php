4f068a927802383852f94fd5754b032e83fdf463
Lukasz Gaszyna <lukaszgaszyna@gmail.com>
2021-05-17 17:09:08 +0200

    Fix page-contact

--- www/resources/views-default/page-contact/show.blade.php
+++ www/resources/views-default/page-contact/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="page-title">{!! $page->title !!}</div>
-    <x-site.line-splitter class="page-description" :text="{!! $page->description !!}"/>
+    <x-site.line-splitter class="page-description" :text="$page->description ?? ''"/>
     <x-site.format-model class="page-text" :model="$page" :nl2br="false"/>{{-- <x-site.format-page :text="$page->text" :images="$page->images" :nl2br="false"/> --}}
     <x-site.contact-full class="in-contact"/>
     <x-site.galleries :feed="$page->galleries"/>
