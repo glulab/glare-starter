9f6618e34e75fe1c21a0d3209b21b3a5043405b8
Lukasz Gaszyna <lukaszgaszyna@gmail.com>
2021-05-18 17:28:02 +0200

    Add parameters to routes function

--- www/routes/web.php
+++ www/routes/web.php
@@ -19,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 
 // Auth::routes(['register' => false, 'login' => false, 'logout' => false, 'reset' => false, 'confirm' => false, 'verify' => false]);
 
-\Glare::routes();
+\Glare::routes(['context', 'site']);
